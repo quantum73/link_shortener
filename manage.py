6b235a4588d43d0170399ba1f5fd63c7bdb2d91c
Nikita Fomin <fominquant@yandex.ru>
2022-04-17 14:26:51 +0300

    - Added manager command for run tests

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 
+import pytest
 from flask_script import Manager, Shell
 
 from app import create_app, db
+from config import basedir
 
 app = create_app(os.environ.get('FLASK_CONFIG', 'default'))
 manager = Manager(app)
@@ -18,8 +20,13 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @manager.command
-def run_tests():
-    pass
+def run_tests(tests_dir=None, verbosity=False):
+    if tests_dir is None:
+        tests_dir = os.path.join(basedir, 'tests')
+    command = [tests_dir]
+    if verbosity:
+        command.append('-v')
+    pytest.main(command)
 
 
 if __name__ == "__main__":
